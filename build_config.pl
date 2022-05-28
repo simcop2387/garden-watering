be4bd6d9cb46b747a62a43dc50fe526df7d268e9
Ryan Voots <simcop2387@simcop2387.info>
2022-05-28 19:58:05 -0400

    more yaml syntax

--- build_config.pl
+++ build_config.pl
@@ -153,14 +153,14 @@ sensor:
     sensors:
       - name: "VEML7700 Light" # Required dummy sensor
       - name: "VEML770 Lux"
-          unit_of_measurement: Lux
-          accuracy_decimals: 0
+        unit_of_measurement: Lux
+        accuracy_decimals: 0
       - name: "VEML770 White"
-          unit_of_measurement: raw
-          accuracy_decimals: 0
+        unit_of_measurement: raw
+        accuracy_decimals: 0
       - name: "VEML770 ALS"
-          unit_of_measurement: raw
-          accuracy_decimals: 0
+        unit_of_measurement: raw
+        accuracy_decimals: 0
   - platform: bme280
     temperature:
       name: "Garden Temperature"
